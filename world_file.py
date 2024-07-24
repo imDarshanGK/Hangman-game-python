32110c6bebf8e70890e91e299686d4ea3411a8f8
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-07-24 19:45:53 +0530

    world_file.py

--- world_file.py
+++ world_file.py
@@ -4,5 +4,6 @@ words = [
     "private", "science", "crystal", "default", "village", "involve", "airport", "outcome", "another", "justice",
     "content", "anxiety", "variety", "notable", "clothes", "restore", "romance", "attempt", "summary", "context",
     "feature", "cabinet", "despite", "extract", "storage", "fitness", "radical", "teacher", "believe", "revenue",
-    "refugee", "natural", "upgrade", "careful", "account", "digital", "various", "benefit", "various", "village"
+    "refugee", "natural", "upgrade", "careful", "account", "digital", "various", "benefit", "various", "village",
+    "ability", "towards", "dispute", "project", "towards", "restore", "dispute", "release", "freedom", "release"
 ]
